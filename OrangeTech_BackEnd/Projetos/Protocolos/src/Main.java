3c5df853b1119377d377b77ca4e05040cc44d2ed
ANGELO FERRO(SIG) <Angelo.sonic@gmail.com>
2023-01-27 14:02:19 -0400

    Att15

---
+++ OrangeTech_BackEnd/Projetos/Protocolos/src/Main.java
@@ -0,0 +1,41 @@
+import java.util.LinkedHashMap;
+import java.util.LinkedHashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class Main {
+    public static void main(String[] args) {
+//        System.out.println("Nao suporto a UFRR");
+//
+//        k=9
+//
+//        System.out.println("XJY CEZYBDY J ENBB");
+//
+//        A=1
+//        B=2
+//        C=3
+//        D=4
+//        E=5
+//        F=6
+//        G=7
+//        H=8
+//        I=9
+//        J=10
+//        k=12
+//        L=13
+//        M=14
+//        N=15
+//        O=16
+//        P=17
+//        Q=18
+//        R=19
+//        S=20
+//        T=21
+//        U=22
+//        V=23
+//        X=24
+//        Y=25
+//        Z=26
+        Map<CharSequence> frase = new LinkedHashMap('n','a','o','s','u');
+    }
+}
