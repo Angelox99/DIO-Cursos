1f2e89333cd407f0f3d542d64d12da9377b36c9b
ANGELO FERRO(SIG) <Angelo.sonic@gmail.com>
2023-02-08 08:38:27 -0400

    Att17

---
+++ OrangeTech_BackEnd/Projetos/Spring/primeiros-passos/src/main/java/dio/springboot/app/Beans.java
@@ -0,0 +1,12 @@
+package dio.springboot.app;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class Beans {
+    @Bean
+    public Gson gson(){
+        return new Gson();
+    }
+}
